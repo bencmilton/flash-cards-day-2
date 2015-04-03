94e7f0feff30303440200e220747c7f021464ef4
ksliao <katrina.s.liao@gmail.com>
2015-04-03 13:38:18 -0400

    Added factory methods

--- public/app.js
+++ public/app.js
@@ -11,33 +11,42 @@ app.controller('FlashCardController', function ($scope) {
     };
 });
 
-app.controller ('MainController', function ($scope) {
+app.factory('FlashCardsFactory', function ($http) {
+     return {
 
-    $scope.flashCards = [
-        {
-            question: 'What is Angular?',
-            answers: [
-                { text: 'A front-end framework for great power!', correct: true },
-                { text: 'Something lame, who cares, whatever.', correct: false },
-                { text: 'Some kind of fish, right?', correct: false }
-            ]
-        },
-        {
-            question: 'What is a controller?',
-            answers: [
-                { text: 'Something that manages my front-end routes', correct: false },
-                { text: 'A function that allows me to manage a scope', correct: true },
-                { text: 'An Angular template', correct: false }
-            ]
-        },
-        {
-            question: 'What does {{ }} do?',
-            answers: [
-                { text: 'It runs some Javascript', correct: false },
-                { text: 'It looks for variables in HTML', correct: false },
-                { text: 'It runs an Angular expression that accesses my $scope', correct: true }
-            ]
+        getFlashCards: function (category) {
+
+            var queryParams = {};
+
+            if (category) {
+                queryParams.category = category;
+            }
+
+            return $http.get('/cards', {
+                params: queryParams
+            }).then(function (response) {
+                console.log(response);
+                return response.data;
+            });
+
         }
-    ];
+
+    };
+});
+
+app.controller ('MainController', function ($scope, FlashCardsFactory) {
+    
+     FlashCardsFactory.getFlashCards().then(function(response){
+        $scope.flashCards = response;
+        
+    });
+
+     $scope.categories = [
+        'MongoDB',
+        'Express',
+        'Angular',
+        'Node'
+     ];
 
 });
+    
